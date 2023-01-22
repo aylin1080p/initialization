38e25345352f4e3c1d69cca15c9b9e6a3f4fabdf
aylin gunes <4ylingunes@gmail.com>
2023-01-22 12:24:55 +0100

    create an initialization function  from a class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-var greeting = "Hello..."
+class Person {
+    var name:String?
+    var age:Int?
+    var weight:Double?
+    var height:Double?
+    
+    init(){ // empty initialization -- when a new object is generated from this class this command will be executed
+        
+    }
+    
+    init(name:String,age:Int,weight:Double,height:Double){
+        //shadowing self.x = x
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+        
+    }
+    
+    
+    
+}
+
+var human = Person()
+human.age = 36
+human.height = 167.4
+human.weight = 48.450
+human.name = "Aylin Güneş"
+
+
+func takeInfo(somebody:Person){ //this method excepts an object of the Person Class
+    print("name: \(somebody.name!)")
+    print("age: \(somebody.age!)")
+    print("height: \(somebody.height!)")
+    print("weight: \(somebody.weight!)")
+    
+}
+
+
+takeInfo(somebody: human)
